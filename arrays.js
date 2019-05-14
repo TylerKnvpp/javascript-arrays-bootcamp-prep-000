5fc564b1d5ac3adf6434cc0230c48b51407b8026
tyler knapp <tgknapp11@gmail.com>
2019-05-14 16:42:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 }
 
  function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  array.unshift(0)
   return array
 }
 
